a176c90f3083c41ae526facee3d570db5c4d092a
Jayanth Shanmugam <mk.jayanth.s@gmail.com>
2026-01-14 13:18:20 -0500

    Add producer class

--- wal2rabbitmq.py
+++ wal2rabbitmq.py
@@ -1,19 +1,64 @@
 import json
+
 import pika
 import psycopg2
 from psycopg2.extras import LogicalReplicationConnection
 
+
+class Producer:
+    def __init__(
+        self, host: str, port: int, username: str, password: str, queue_name: str
+    ):
+        self.host = host
+        self.port = port
+        self.username = username
+        self.password = password
+        self.queue_name = queue_name
+        self.connection = None
+        self.channel = None
+
+    def __enter__(self):
+        self.connection = pika.BlockingConnection(
+            pika.ConnectionParameters(
+                host=self.host,
+                port=self.port,
+                credentials=pika.PlainCredentials(self.username, self.password),
+            )
+        )
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue=self.queue_name)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.connection and not self.connection.is_closed:
+            self.connection.close()
+
+    def publish(self, message: dict):
+        self.channel.basic_publish(
+            exchange="", routing_key=self.queue_name, body=json.dumps(message)
+        )
+
+
 class Consumer:
-    def __init__(self, host: str, port: int, database: str, username: str, password: str):
+    def __init__(
+        self,
+        host: str,
+        port: int,
+        database: str,
+        username: str,
+        password: str,
+        producer: Producer,
+    ):
         self.host = host
         self.port = port
         self.database = database
         self.username = username
         self.password = password
-    
+        self.producer = producer
+
     def consume(self, slot_name: str, options: dict, decode: bool) -> dict:
-        '''
-        Core consumer method. Connects to the database through a 
+        """
+        Core consumer method. Connects to the database through a
         logical replication connection, waits for incoming WAL
         changes and publishes them to a RabbitMQ queue.
 
@@ -23,23 +68,23 @@ class Consumer:
             decode: Decode flag for the slot
         Returns:
             None
-        '''
+        """
 
         conn = psycopg2.connect(
-            dbname = self.dbname,
-            user = self.user,
-            password = self.password,
-            host = self.host,
-            port = self.port,
-            connection_factory = LogicalReplicationConnection
+            dbname=self.database,
+            user=self.username,
+            password=self.password,
+            host=self.host,
+            port=self.port,
+            connection_factory=LogicalReplicationConnection,
         )
 
         cur = conn.cursor()
 
         cur.start_replication(
-            slot_name = slot_name,
-            options = options,
-            decode = decode,
+            slot_name=slot_name,
+            options=options,
+            decode=decode,
         )
 
         while True:
@@ -49,10 +94,8 @@ class Consumer:
                     msg_payload = json.loads(msg.payload)
                     db_changes = msg_payload.get("change")
                     for change in db_changes:
-                        # Publish changes to a RabbitMQ queue
+                        self.producer.publish(change)
                 else:
-                    print("\rListening for WAL changes...", end = "", flush = True)
+                    print("\rListening for WAL changes...", end="", flush=True)
             except KeyboardInterrupt:
                 print("Closing replication connection...")
-                cur.stop_replication()
-        
